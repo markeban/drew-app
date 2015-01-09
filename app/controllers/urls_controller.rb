917d673957706d6c1e7dbd9a93611976d85bf279
Mark Richardson <markeban@yahoo.com>
2015-01-09 06:40:25 -0600

    first commit

---
+++ app/controllers/urls_controller.rb
@@ -0,0 +1,53 @@
+class UrlsController < ApplicationController
+require 'csv'
+require 'json'
+
+
+  def index
+  end
+
+  def results
+    api_calls = Hash.new
+    @json_and_placeholder = []
+
+    url = params[:url]
+    placeholders = params[:placeholder].gsub(/\s+/m, ' ').strip.split(" ")
+
+    placeholders.each do |placeholder|
+      api_calls[url.sub('placeholder', placeholder)] = placeholder
+    end
+    
+    api_calls.each do |url, placeholder|
+      json_data = Unirest.get(url).body
+      json_data.each do |key, array|
+        array.each do |hash|
+          hash["placeholder"] = placeholder
+          @json_and_placeholder.push(hash)
+        end
+      end
+    end
+
+
+
+
+
+    headers = %w{actor_name object_id translated_event_type date_time_in_timezone placeholder}
+    file_name = "drew_data"
+
+    csv_file = CSV.generate do |csv|
+      csv << headers
+      @json_and_placeholder.each do |hash|
+        csv << headers.map { |h| hash[h] }
+      end
+    end
+    send_data csv_file, :type => 'text/csv; charset=iso-8859-1; header=present', :disposition => "attachment;data=#{file_name}.csv"
+
+  end
+
+  private
+
+  def url_params
+    params.require(:url).permit(:url, :placeholder)
+  end
+
+end
